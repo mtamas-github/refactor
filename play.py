4ac6e1aaf1f16f3994fa3bb56786b33774bc3c71
Tamas <tamas@matacro.co.uk>
2024-02-01 13:45:27 +0100

    initial code

---
+++ play.py
@@ -0,0 +1,12 @@
+from yatzy import Yatzy
+
+dice = Yatzy.generate_dice()
+chance = Yatzy.chance(dice[0], dice[1], dice[2], dice[3], dice[4])
+pair = Yatzy.score_pair(dice[0], dice[1], dice[2], dice[3], dice[4])
+yatzy = Yatzy.yatzy(dice)
+threeofakind = Yatzy.three_of_a_kind(dice[0], dice[1], dice[2], dice[3], dice[4])
+
+print(chance)
+print(pair)
+print(yatzy)
+print(threeofakind)
