3204f766cae21b5a8710d54d53285cc4ad5b875b
mtamas-github <51745124+mtamas-github@users.noreply.github.com>
2025-02-25 15:51:16 +0100

    Update play.py

--- play.py
+++ play.py
@@ -6,6 +6,8 @@ pair = Yatzy.score_pair(dice[0], dice[1], dice[2], dice[3], dice[4])
 yatzy = Yatzy.yatzy(dice)
 threeofakind = Yatzy.three_of_a_kind(dice[0], dice[1], dice[2], dice[3], dice[4])
 
+
+# some example calls.
 print(chance)
 print(pair)
 print(yatzy)
